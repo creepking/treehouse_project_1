c508a72a2aa113d527d37a45d9853203000af60b
Scott Miller <scott@scottmillerrealestate.com>
2019-02-21 18:38:12 -0500

    updated js file
    redid the getRandomQuote and printQuote functions based on feedback.

--- script.js
+++ script.js
@@ -5,29 +5,29 @@ chosen quotes and sources from Parade website
 var quotes= [
 
       {
-        quoteOne: <p class="quote">'“Never be limited by other people\’s limited imaginations."' </p>
-        sourceOne: <p class="source">'"-Dr. Mae Jemison, first African-American female astronaut"'</p>
+        quote: '“Never be limited by other people\’s limited imaginations."',
+        source:"Dr. Mae Jemison, first African-American female astronaut"
       },
 
       {
-        quoteTwo: <p class="quote">'“The cost of liberty is less than the price of repression."'</p>,
-        sourceTwo: <p class="source">"-W.E.B. Du Bois"/p>
+        quote: '“The cost of liberty is less than the price of repression."',
+        source: "W.E.B. Du Bois"
 
       },
 
       {
-        quoteThree: <p class="quote">'“Hate is too great a burden to bear. It injures the hater more than it injures the hated.”'</p>,
-        sourceThree:<p class="source">"-Coretta Scott King"</p>
+        quote: '“Hate is too great a burden to bear. It injures the hater more than it injures the hated.”',
+        source: "Coretta Scott King"
       },
 
       {
-        quoteFour: <p class="quote">'“Whatever we believe about ourselves and our ability comes true for us.”'</p>,
-        sourceFour: <p class="source"> "-Susan L. Taylor, journalist"</p>
+        quote: '“Whatever we believe about ourselves and our ability comes true for us.”',
+        source: "Susan L. Taylor, journalist"
       },
 
       {
-        quoteFive: <p class="quote"> '“If there is no struggle, there is no progress.”'</p>,
-        sourceFive: <p class="source"> "-Frederick Douglass"</p>
+        quote: '“If there is no struggle, there is no progress.”',
+        source: "Frederick Douglass"
       }
 
 ];
@@ -35,34 +35,13 @@ var quotes= [
 // getRandomQuote function calculates a random number between 1 and 5, then
 // selects a quote with the corresponding spot in the array.
 
-function getRandomQuote() {
-
-  var randomNumber=Math.floor(Math.random()*quotes.length);
-      if (randomNumber==1) {
-        return quotes[0].quoteOne + " " + quotes[0].sourceOne;
-      }
-
-  else if (randomNumber==2) {
-        return quotes[1].quoteTwo + "  " + quotes[1].sourceTwo;
-      }
-
-  else if (randomNumber==3) {
-        return quotes[2].quoteThree + "  " + quotes[2].sourceThree;
-      }
-
-  else if (randomNumber==4) {
-        return quotes[3].quoteFour + "  " + quotes[3].sourceFour;
-      }
-
-  else {
-        return quotes[4].quoteFive + "  " + quotes[4].sourceFive;
-      }
-
-
+function getRandomQuote(quoteSelection) {
+var randomNumber=Math.floor(Math.random()*quotes.length);
+  for (i=0; i<quotes.length; i++) {
+    var randomQuote=quoteSelection[randomNumber];
+  }
+  return randomQuote;
 }
-
-
-
 
 /***
   The printQuote function calls the getRandomQuote function and gives it the
@@ -70,13 +49,16 @@ function getRandomQuote() {
 ***/
 
 function printQuote(){
-  chosenQuote=getRandomQuote();
-  document.getElementById("quote-box").innerHTML = chosenQuote;
-  return chosenQuote;
+  var message = " ";
+  var chosenQuote= getRandomQuote(quotes);
+  message= "<p class='quote'>" + chosenQuote.quote + "</p>";
+  message+= "<p class='source'>" + chosenQuote.source;
+  message+="</p>";
+
+  document.getElementById("quote-box").innerHTML = message;
 }
 
-
-
+printQuote();
 
 /***
   the event listener below invokes the printQuote function when the button is
