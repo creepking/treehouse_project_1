2e15119627ccf180345ba479f83045c0b78513b2
Scott Miller <scott@scottmillerrealestate.com>
2019-02-20 19:13:48 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,56 @@
 /******************************************
-chosen quotes and sources from Parade website
+Treehouse Techdegree:
+FSJS project 1 - A Random Quote Generator
+******************************************/
+
+// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+
+
+/***
+  Create the array of quote objects and name it `quotes`.
+  Add at least five quote objects to the `quotes` array.
+  Give each quote object a `quote` and `source` property.
+
+  Recommended:
+    - Add at least one `year` and/or `citation` property to at least one
+      quote object.
 ***/
 
 var quotes= [
-
       {
-        quoteOne: '“Never be limited by other people\’s limited imaginations."',
-        sourceOne: '<br>' + "-Dr. Mae Jemison, first African-American female astronaut"
+        quote1: '“Never be limited by other people’s limited imaginations.”',
+        source1: "Dr. Mae Jemison, first African-American female astronaut"
+},
+      {
+        quote2: '“The cost of liberty is less than the price of repression."',
+        source2: "—W.E.B. Du Bois",
+
+      },
+      {
+        quote3: '“Hate is too great a burden to bear. It injures the hater more than it injures the hated.”',
+        source3: "—Coretta Scott King"
       },
 
       {
-        quoteTwo: '“The cost of liberty is less than the price of repression."',
-        sourceTwo: '<br>' + "-W.E.B. Du Bois"
-
+        quote4: '“Whatever we believe about ourselves and our ability comes true for us.” ',
+        source4: "—Susan L. Taylor, journalist"
       },
 
       {
-        quoteThree: '“Hate is too great a burden to bear. It injures the hater more than it injures the hated.”',
-        sourceThree: '<br>'+"-Coretta Scott King"
-      },
-
-      {
-        quoteFour: '“Whatever we believe about ourselves and our ability comes true for us.”',
-        sourceFour: '<br>' + "-Susan L. Taylor, journalist"
-      },
-
-      {
-        quoteFive: '“If there is no struggle, there is no progress.”',
-        sourceFive: '<br>' + "-Frederick Douglass"
+        quote5: '“If there is no struggle, there is no progress.” ',
+        source5: "—Frederick Douglass"
       }
 
 ];
 
-// getRandomQuote function calculates a random number between 1 and 5, then
-// selects a quote with the corresponding spot in the array.
+
+/***
+  Create the `getRandomQuote` function to:
+   - generate a random number
+   - use the random number to `return` a random quote object from the
+     `quotes` array.
+***/
+
 
 function getRandomQuote() {
 
@@ -65,8 +82,13 @@ function getRandomQuote() {
 
 
 /***
-  The printQuote function calls the getRandomQuote function and gives it the
-  variable name chosenQuote, then adds the result to the innerHTML "quote-box"
+  Create the `printQuote` function to:
+   - call the `getRandomQuote` function and assign it to a variable.
+   - use the properties of the quote object stored in the variable to
+     create your HTML string.
+   - use conditionals to make sure the optional properties exist before
+     they are added to the HTML string.
+   - set the `innerHTML` of the `quote-box` div to the HTML string.
 ***/
 
 function printQuote(){
@@ -79,8 +101,13 @@ function printQuote(){
 
 
 /***
-  the event listener below invokes the printQuote function when the button is
-  clicked, changing the quote based on the randomly generated result.
+  When the "Show another quote" button is clicked, the event listener
+  below will be triggered, and it will call, or "invoke", the `printQuote`
+  function. So do not make any changes to the line of code below this
+  comment.
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
